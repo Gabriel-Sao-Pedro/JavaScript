5d4da78f8c2b55f51a2a432db7e6c63c79bce17e
Gabriel São Pedro <gabrielosaopedro@gmail.com>
2025-09-02 15:18:05 -0300

    Refactor project structure and clean up files

--- JavaScript Basico/dom_pt3/dom_pt3.js
+++
@@ -1,5 +0,0 @@
-function modificarEstilo(corDeFundo, l, a){
-    document.getElementById('quadrado').style.backgroundColor = corDeFundo;
-    document.getElementById('quadrado').style.width = l;
-    document.getElementById('quadrado').style.height = a
-}
